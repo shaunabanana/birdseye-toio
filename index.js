f0697386de850c098fa12d2a48e623db751ec231
Shengchen Zhang <shengchenzhang1207@gmail.com>
2021-10-04 20:21:53 +0800

    Finished toio management

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Cube } = require('@toio/cube')
 const noble = require('@abandonware/noble');
+const { WebSocketServer, WebSocket } = require('ws');
+const KalmanFilter = require('kalmanjs');
 
 
 let ToioConstants = {
@@ -17,9 +19,38 @@ let ToioConstants = {
 
 class CubeManager {
 
-    constructor () {
+    constructor(bypassWebSocket) {
         this.cubes = {};
+        this.events = [];
 
+        // Kalman filters to deal with noise in toio sensing
+        this.xFilter = new KalmanFilter();
+        this.yFilter = new KalmanFilter();
+        this.angleFilter = new KalmanFilter();
+
+        this.server = new WebSocketServer({ port: 8175 });
+
+        if (bypassWebSocket) {
+            this.connect();
+        } else {
+            console.log('Waiting for the frontend to connect...');
+            this.server.on('connection', () => {
+                console.log('WebSocket connected.');
+                this.connect();
+            });
+        }
+        
+    }
+
+    broadcast(message) {
+        this.server.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(message));
+            }
+        });
+    }
+
+    connect() {
         noble.on('stateChange', state => {
             if (state === 'poweredOn') {
                 console.log('BLE module powered on. Scanning for TOIO robots...')
@@ -30,16 +61,108 @@ class CubeManager {
         noble.on('discover', async (peripheral) => {
             let cube = new Cube(peripheral);
             await cube.connect();
-            
+
             if (!this.cubes[cube.peripheral.advertisement.localName]) {
-                this.cubes[cube.peripheral.advertisement.localName] = cube;
-                cube.on('sensor:orientation', data => console.log(data));
-                cube.on('sensor:double-tap', data => console.log('Double tap'));
-                cube.on('sensor:slope', data => console.log(data));
-                cube.on('sensor:collision', data => console.log(data));
+                let cubeObject = {
+                    name: cube.peripheral.advertisement.localName,
+                    control: cube,
+                    orientation: 1,
+                    x: 0,
+                    y: 0,
+                    angle: 0,
+                    lastUpdate: 0
+                };
+                this.cubes[cube.peripheral.advertisement.localName] = cubeObject;
+                this.registerEventHandlers(cubeObject);
                 console.log('Connected to cube', cube.peripheral.advertisement.localName);
+                this.broadcast({
+                    'event': 'toio:connect',
+                    'name': cubeObject.name
+                })
             }
         });
+    }
+
+    registerEventHandlers(cube) {
+        cube.control.on('id:position-id', data => {
+            this.onMove(cube, data);
+        });
+        cube.control.on('sensor:orientation', data => {
+            cube.orientation = data.orientation
+        });
+        cube.control.on('sensor:double-tap', this.onDoubleTap.bind(this, cube));
+        // cube.control.on('sensor:slope', data => console.log(data));
+        // cube.control.on('sensor:collision', data => console.log(data));
+    }
+
+    onDoubleTap(cube) {
+        let eventTime = Date.now();
+        this.events.push({
+            time: eventTime,
+            type: 'sensor:double-tap',
+            cube: cube.name
+        })
+        for (let event of this.events) {
+            if (event.cube !== cube.name
+                && event.type === 'sensor:double-tap'
+                && Math.abs(event.time - eventTime) < 100) {
+
+                if (this.cubes[event.cube].orientation === 1 && this.cubes[cube.name].orientation === 1) {
+                    console.log('Linking', event.cube, 'and', cube.name);
+                    this.broadcast({
+                        'event': 'toio:link',
+                        'from': event.cube,
+                        'to': cube.name,
+                    })
+                } else if (this.cubes[event.cube].orientation === 2 && this.cubes[cube.name].orientation === 1) {
+                    console.log('Emptying', event.cube, 'on', cube.name);
+                    this.broadcast({
+                        'event': 'toio:empty',
+                        'from': event.cube,
+                        'to': cube.name,
+                    })
+                } else if (this.cubes[event.cube].orientation === 1 && this.cubes[cube.name].orientation === 2) {
+                    console.log('Emptying', cube.name, 'on', event.cube);
+                    this.broadcast({
+                        'event': 'toio:empty',
+                        'from': cube.name,
+                        'to': event.cube,
+                    })
+                }
+                this.events = [];
+                return;
+            }
+        }
+
+        setTimeout(() => {
+            if (this.events.length === 0) return;
+            for (let event of this.events) {
+                if (event.cube !== cube.name
+                    && event.type === 'sensor:double-tap'
+                    && Math.abs(event.time - eventTime) < 100) {
+                    this.events = [];
+                    return;
+                }
+            }
+            console.log('Double-tapping', cube.name);
+            this.broadcast({
+                'event': 'toio:expand',
+                'cube': cube.name
+            })
+        }, 100);
+    }
+
+    onMove(cube, data) {
+        cube.x = this.xFilter.filter(data.x);
+        cube.y = this.yFilter.filter(data.y);
+        cube.angle = this.angleFilter.filter(data.angle);
+
+        this.broadcast({
+            'event': 'toio:move',
+            'cube': cube.name,
+            'x': cube.x, 'y': cube.y, 
+            'angle': cube.angle
+        })
     }
 
 }
